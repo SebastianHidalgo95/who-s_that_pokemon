e3e56486dc3444b32a8ff81694a5fe28932e517a
Sebastian Hidalgo <sebastianh.ing@gmail.com>
2022-03-14 22:35:59 -0500

    First commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <PokemonPicture :pokemonId="150" :showPokemon="false"/>
+    <PokemonOptions />
+</template>
+<script>
+
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
+
+export default {
+    name: 'PokemonPage',
+    components: { PokemonOptions, PokemonPicture }
+}
+</script>
+<style lang="">
+    
+</style>
